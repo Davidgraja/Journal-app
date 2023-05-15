eb21448167544d53cf5614b2dcae088b6eb0df31
David gp <juliograjalesparedes127@gmail.com>
2023-05-15 22:07:36 +0200

    finalizando  la maquetación de las paginas de login y registro

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,3 @@
-// import { Route, Routes } from "react-router-dom"
-// import { LoginPage } from "./auth/pages/LoginPage"
-// import { RegisterPage } from "./auth/pages/RegisterPage"
-
 import { AuthRouter } from "./auth/router/AuthRouter"
 
 const App = () => {
